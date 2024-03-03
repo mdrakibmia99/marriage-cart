0a7c4c3933ce5f771b669b3c77a9d7069af73169
mdrakibmia99 <rkrakibhasan680@gmail.com>
2024-03-03 10:02:02 +0600

    update

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-"use client"
 import Image from 'next/image'
 import ContactDetail from "@/components/details/ContactDetail";
 import FamilyDetail from "@/components/details/FamilyDetail";
@@ -8,77 +7,45 @@ export default function Home() {
   const backgroundColor = 'rgb(255, 250, 231)';
   const A4Width = 794; // A4 width in pixels (210mm converted to pixels)
   const A4Height = 1123; // A4 height in pixels (297mm converted to pixels)
-  const bio_bg='rgb(186 140 65);'
+  const bio_bg = 'rgb(186 140 65);'
 
 
-// pdf crea
+  return (
+    <main>
 
-
-
-    return (
       <div className="flex flex-col  justify-center items-center gap-5">
-      <div className="p-10" style={{ backgroundColor, width: `${A4Width}px`, height: `${A4Height}px` }}>
+        <div className="p-10" style={{ backgroundColor, width: `${A4Width}px`, height: `${A4Height}px` }}>
         <div className="w-full h-full border-8 border-[#cfaf61] relative">
-
-           <div className="flex justify-center items-center flex-col py-10">
-             <p className="text-2xl font-bold text-[#be9b5b]">BIO DATA</p>
-             <div className={`w-36 h-36 bg-[#ba8c41] rounded-full flex justify-center items-center `}>
-             <FaUser  className="w-1/2 h-1/2 text-slate-50"/>
-             </div>
-           </div>
-          <div  className="w-4/5 mx-auto flex flex-col gap-7 pt-10">
-
-           <PersonalDetail/>
-           <FamilyDetail/>
-           <ContactDetail/>
-          </div>
-
-             <div className='absolute top-0 left-0 '>
-              <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
-             </div>
-             <div className='absolute top-0 right-0 rotate-90'>
-              <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
-             </div>
-             <div className='absolute bottom-0 left-0 -rotate-90'>
-              <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
-             </div>
-             <div className='absolute bottom-0 right-0 -rotate-180'>
-              <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
-             </div>
-     
+        
+        <div className="flex justify-center items-center flex-col py-10">
+        <p className="text-2xl font-bold text-[#be9b5b]">BIO DATA</p>
+        <div className={`w-36 h-36 bg-[#ba8c41] rounded-full flex justify-center items-center `}>
+        <FaUser  className="w-1/2 h-1/2 text-slate-50"/>
         </div>
-      </div>
-      <div className="p-10" style={{ backgroundColor, width: `${A4Width}px`, height: `${A4Height}px` }}>
-        <div className="w-full h-full border-8 border-[#cfaf61] relative">
-
-           <div className="flex justify-center items-center flex-col py-10">
-             <p className="text-2xl font-bold text-[#be9b5b]">BIO DATA</p>
-             <div className={`w-36 h-36 bg-[#ba8c41] rounded-full flex justify-center items-center `}>
-             <FaUser  className="w-1/2 h-1/2 text-slate-50"/>
-             </div>
-           </div>
-          <div  className="w-4/5 mx-auto flex flex-col gap-7 pt-10">
-
-           <PersonalDetail/>
-           <FamilyDetail/>
-           <ContactDetail/>
-          </div>
-
-             <div className='absolute top-0 left-0 '>
-              <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
-             </div>
-             <div className='absolute top-0 right-0 rotate-90'>
-              <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
-             </div>
-             <div className='absolute bottom-0 left-0 -rotate-90'>
-              <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
-             </div>
-             <div className='absolute bottom-0 right-0 -rotate-180'>
-              <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
-             </div>
-     
+        </div>
+        <div  className="w-4/5 mx-auto flex flex-col gap-7 pt-10">
+        
+        <PersonalDetail/>
+        <FamilyDetail/>
+        <ContactDetail/>
+        </div>
+        
+        <div className='absolute top-0 left-0 '>
+        <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
+        </div>
+        <div className='absolute top-0 right-0 rotate-90'>
+        <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
+        </div>
+        <div className='absolute bottom-0 left-0 -rotate-90'>
+        <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
+        </div>
+        <div className='absolute bottom-0 right-0 -rotate-180'>
+        <Image src={'/images/left-top.png'} width={100} height={100} alt='' />
+        </div>
+        
         </div>
       </div>
       </div>
-    );
-  }
+    </main>
+  );
+}
---
+++ components/pdf/PrintablePage.js
@@ -0,0 +1,12 @@
+'use client'
+// components/PrintablePage.js
+
+import styled from 'styled-components';
+
+const PrintablePage = styled.div`
+  width: 210mm;
+  min-height: 297mm;
+  /* Other styles can be added here */
+`;
+
+export default PrintablePage;
